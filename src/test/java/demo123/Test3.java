32c84ec356d0840e9b0627642fdb9de8ea00bd66
QSP <QSP@QSP>
2021-04-01 09:34:20 +0530

    hello

---
+++ src/test/java/demo123/Test3.java
@@ -0,0 +1,14 @@
+package demo123;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+@Test
+public void demo() {
+	String s="appium";
+	Assert.assertEquals(s,"selenium");
+	
+}
+}
